5f2fb8368637c12bc04c48850cc6ee638853e1dd
Meaghan Jones and Olivia Hinton <olivia@>
2016-06-02 09:22:09 -0700

    add js if, else statements to make sections display

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var animal = prompt("Do you want to learn about turtles, snakes or insects?");
+
+  if (animal === "turtles") {
+    $('#turtles').show();
+  } else if (animal === "turtle"){
+    $('#turtles').show();
+  } else if (animal === "snakes"){
+    $('#snakes').show();
+  } else if (animal === "snake"){
+    $('#snakes').show();
+  } else if (animal === "insects"){
+    $('#insects').show();
+  } else if (animal === "insect"){
+    $('#insects').show();
+  }
+
+});
